09f207b1b857578844571bfd14987a1b525a3802
cookieisgood <alan.yuan.jly@gmail.com>
2017-10-22 18:49:15 -0400

    Completed the Alice throught the looking glass challenge
    forgot to exit loop after the leaves stoped going up.
    finally got 100%

--- CCC '11 S3 - Alice Through the Looking Glass.cpp
+++ CCC '11 S3 - Alice Through the Looking Glass.cpp
@@ -1,6 +1,9 @@
+/**
+ complete
+ 
 #include <iostream>
 using namespace std;
-
+//link: https://dmoj.ca/problem/ccc11s3
 uint power(uint num, int toPowOf){
     uint mult = num;
     if(toPowOf == 0){
@@ -23,24 +26,39 @@ int main(int argc, const char * argv[]) {
     uint y = 0;
     
     cin>>enterAmount;
+    bool crystal[enterAmount];
+    
     for (uint i = 0; i < enterAmount; i++) {
         cin>>m>>x>>y; // 2 4 4
-        long long highestY = 0;
-        for (uint im = 0; im < m; im++) {
-            uint max = power(5, m-im-1);
-            uint xPos = x/max;
-            highestY+=multipliers[xPos] * max;
-            x = x%max;
+        uint highestY = 0;
+        for (int im = m; im > 0; im--) {
+            uint oneTile = power(5, im-1); // check how big one tile is
+            
+            // check where x would be if it only the bigest tiles (divided into five)
+            uint xPos = x/oneTile;
+            highestY+=multipliers[xPos] * oneTile; // change tallest point
+            if(multipliers[xPos] == 0){
+                im=0; // if this section no longer goes up, end the loop
+            }
+            x = x%oneTile; // x gets everthing outside of the current tile removed
+            
         }
         
         if(y<highestY){
-            cout<<"crystal\n";
+            crystal[i] = true;
         }else{
-            cout<<"empty\n";
+            crystal[i] = false;
         }
         
     }
     
-    
+    for (uint i = 0; i < enterAmount; i++) {
+        if(crystal[i]){
+            cout<<"crystal\n";
+        }else{
+            cout<<"empty\n";
+        }
+    }
     return 0;
 }
+ **/
